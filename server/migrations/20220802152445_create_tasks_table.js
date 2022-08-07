29a5df6eb578df0c22ffaa2013f897aabed69209
eliza <elizabet002@mail.ru>
2022-08-07 18:24:18 +0300

    fix linter errs

--- server/migrations/20220802152445_create_tasks_table.js
+++ server/migrations/20220802152445_create_tasks_table.js
@@ -1,30 +1,30 @@
 export const up = (knex) => (
-    knex.schema.createTable('tasks', (table) => {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('description');
-  
-      table
-        .integer('status_id')
-        .unsigned()
-        .index()
-        .references('statuses.id');
-  
-      table
-        .integer('creator_id')
-        .unsigned()
-        .index()
-        .references('users.id');
-  
-      table
-        .integer('executor_id')
-        .unsigned()
-        .index()
-        .references('users.id');
-  
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    })
+  knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('description');
+
+    table
+      .integer('status_id')
+      .unsigned()
+      .index()
+      .references('statuses.id');
+
+    table
+      .integer('creator_id')
+      .unsigned()
+      .index()
+      .references('users.id');
+
+    table
+      .integer('executor_id')
+      .unsigned()
+      .index()
+      .references('users.id');
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
 );
-  
+
 export const down = (knex) => knex.schema.dropTable('tasks');
--- server/migrations/20220804163050_create_labels_table.js
+++ server/migrations/20220804163050_create_labels_table.js
@@ -1,10 +1,10 @@
 export const up = (knex) => (
-    knex.schema.createTable('labels', (table) => {
-      table.increments('id').primary();
-      table.string('name');
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    })
+  knex.schema.createTable('labels', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
 );
-  
+
 export const down = (knex) => knex.schema.dropTable('labels');
